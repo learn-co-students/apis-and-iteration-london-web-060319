7023e53f4038d538d91b16cb5db4c08a7e30de86
github username <github email address>
2019-06-04 16:52:11 +0100

    Completed Star Wars API task

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+# require "pry"
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to Star Wars Character Facts..."
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp.downcase
 end
