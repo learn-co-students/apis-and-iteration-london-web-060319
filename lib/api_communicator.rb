bcc8a02f3a417bb4981c3db3a965e50570fdb6cb
Federica Macchi <federica.macchi3@gmail.com>
2019-06-04 19:28:33 +0100

    Solved

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,21 +6,37 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
+  response_hash["results"].each do |character| 
+    if character["name"] == character_name
+      return character["films"]
+    end
+  end
+end
+# iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
+
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+
+
+def print_movies(films) # some iteration magic and puts out the movies in a nice list
+  films.each do |link|
+    movies_string = RestClient.get("#{link}")
+    movies = JSON.parse(movies_string)
+    puts movies["title"]
+  end
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
+  
+
+
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
@@ -31,3 +47,4 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
+puts "May the force be with you"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  gets.chop
   # use gets to capture the user's input. This method should return that input, downcased.
 end
